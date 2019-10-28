f5491f4dced4df464a16cb776bb43b5dfedcefec
DAMILOLA <jogungbure@gmail.com>
2019-10-28 11:54:51 +0100

    Adding the Tracklisting Component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,8 +9,6 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
   constructor(private _http: Http) {}
   getAlbum(id: number): Observable<Album> {
-    return this._http
-      .get(this._albumUrl)
-      .map(response => <Album>response.json());
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
