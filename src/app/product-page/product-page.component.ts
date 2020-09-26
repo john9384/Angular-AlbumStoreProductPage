af577d8d69782f59a0e42f6d18986d21f543cb1c
Damie <jogungbure@gmail.com>
2020-09-26 12:57:29 +0100

    The Product Description Component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-product-page",
   templateUrl: "./product-page.component.html",
-  styleUrls: ["./product-page.component.css"]
+  styleUrls: ["./product-page.component.css"],
 })
 export class ProductPageComponent implements OnInit {
   constructor() {}
